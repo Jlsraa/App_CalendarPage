a5d06144679308b23753d1c0d7e39c5f08277e0f
alberto romano <aromanog1@gmail.com>
2022-05-04 17:45:06 -0500

    No changes

---
+++ ios/Flutter/flutter_export_environment 2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/alberto/My_Docs/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/alberto/Documents/Primavera 2022/Desarrrollo de Aplicaciones Móviles/App_CalendarPage"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/alberto/Documents/Primavera 2022/Desarrrollo de Aplicaciones Móviles/App_CalendarPage/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/alberto/Documents/Primavera 2022/Desarrrollo de Aplicaciones Móviles/App_CalendarPage/.dart_tool/package_config.json"
